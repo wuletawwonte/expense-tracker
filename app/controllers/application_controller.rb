194133ff38314abe02bb61bdb42b9778efe0f8ca
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-08-10 10:58:45 +0300

    Fix sign_in redirection error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    categories_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
 end
