42f0f3aa3b778c1589f935d955d816e3abce5269
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-08-10 11:28:35 +0300

    Fix navbar elements spacing problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
     categories_path
   end
 
+  def after_sign_up_path_for(resource)
+    categories_path
+  end
+
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
