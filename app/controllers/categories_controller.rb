791fd6d987ad4900936430d48077948f4fb58528
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-08-11 21:20:21 +0300

    Implement category registration feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,32 @@
 class CategoriesController < ApplicationController
-  def index; end
+  def index
+    @categories = Category.where(user_id: current_user.id)
+  end
   
   def new
     @category = Category.new
   end
 
-  def create; end
+  def create
+    @category = Category.new(category_params)
+    @category.user_id = current_user.id
+
+    respond_to do |format|
+      format.html do
+        if @category.save
+          flash[:success] = 'Category successfully saved'
+          redirect_to categories_path
+        else 
+          flash[:error] = 'Unable to save Category'
+          render new
+        end
+      end
+    end
+  end
+
+  private 
+
+  def category_params
+    params.require(:category).permit(:icon, :name)
+  end
 end
