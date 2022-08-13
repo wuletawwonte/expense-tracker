12e3c69e345432fa6aa2d0c7e7f55dadcf2abd4b
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-08-13 15:24:45 +0300

    Initialize RSpec in the repo and write tests for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  subject { User.create(name: 'Wuletaw Wonte', email: 'wuletawwonte@gmail.com', password: '123456') }
+
+  before { subject.save }
+
+  it 'user subject should be valid' do
+    expect(subject).to be_valid
+  end
+
+  describe 'check for validations' do
+    it 'invalidate without name' do
+      subject.name = ''
+      expect(subject).to_not be_valid
+    end
+
+    it 'contains the right name' do
+      subject.name = 'Wuletaw Wonte'
+      expect(subject).to be_valid
+    end
+
+    it 'invalid without password' do
+      subject.password = '123456'
+      expect(subject).to be_valid
+    end
+  end
+end
