83e9448a3f434d53bd9c6f998fe4da455af537eb
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-08-13 16:35:10 +0300

    Add capybara test for the splash screen

---
+++ spec/views/splash_page_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Show splash page: ', type: :feature do
+  before(:each) do
+    visit "/"
+  end
+
+  it 'shows the name of the application' do
+    expect(page).to have_content('expense tracker')
+  end
+
+  it 'shows the login link' do
+    expect(page).to have_content('LOGIN')
+  end
+
+  it 'shows the sign up link' do
+    expect(page).to have_content('SIGN UP')
+  end
+
+end
